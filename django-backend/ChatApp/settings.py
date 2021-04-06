0af08d6891cc5773bafc453e592b0c708c1aa699
billafy <66407462+billafy@users.noreply.github.com>
2021-04-06 15:33:28 +0530

    Update settings.py

--- django-backend/ChatApp/settings.py
+++ django-backend/ChatApp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8n0459ugkx%)*3%pd@qxvd5&6$%dt(z)c7u3l-nvrae@sl8nq^'
+SECRET_KEY = 'DUMMY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
